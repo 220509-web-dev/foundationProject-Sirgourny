27d14e307e0b0803e7e16674f81f7f6a6fa3ab48
Sirgourny Compton <sirgournycompton@Sirgournys-iMac.local>
2022-06-04 17:11:34 -0500

    Updated my ConnectionFactory(not complete), and added an application.properties file to resources

--- src/main/java/com/revature/foundations/app.java
+++ src/main/java/com/revature/foundations/app.java
@@ -1,12 +1,13 @@
 package com.revature.foundations;
 
-import com.revature.foundations.utils.ConnectionUtil;
+import com.revature.foundations.utils.ConnectionFactory;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 public class app {
     public static void main(String[] args) throws SQLException {
-        Connection conn = ConnectionUtil.getConnection();
+        Connection conn = ConnectionFactory.getInstance().getConnection();
     }
 }
