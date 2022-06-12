cde96a8c6079006b9cb1f7c56200748d8824b435
Sirgourny Compton <sirgournycompton@Sirgournys-iMac.local>
2022-06-12 18:01:43 -0500

    Update to pom.xml. Had jackson-core, but was missing jackson-databind

--- foundationProject-Sirgourny/src/main/java/com/revature/foundations/models/User.java
+++ foundationProject-Sirgourny/src/main/java/com/revature/foundations/models/User.java
@@ -11,15 +11,12 @@ public class User {
         private String username;
         private String password;
 
-    public User() {}
+        // The Jackson mapping utility needs to have access to a no-args constructor
+        public User() {
+            super();
+        }
 
     public User(int user_id, String firstname, String lastname, String email, String username, String password) {
-        this.user_id = user_id;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.username = username;
-        this.password = password;
     }
 
     public int getUser_id() {
@@ -55,7 +52,7 @@ public class User {
     }
 
     public String getUsername() {
-        return this.username;
+        return username;
     }
 
     public void setUsername(String username) {
@@ -71,18 +68,6 @@ public class User {
     }
 
     @Override
-    public String toString() {
-        return "User{" +
-                "user_id=" + user_id +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", email='" + email + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -95,6 +80,18 @@ public class User {
         return Objects.hash(user_id, firstname, lastname, email, username, password);
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
     public Object getQuestionText() {
         return null;
     }
@@ -102,4 +99,96 @@ public class User {
     public Object getAnswerText() {
         return null;
     }
+
+    //    public User() {}
+//
+//    public User(int user_id, String firstname, String lastname, String email, String username, String password) {
+//        this.user_id = user_id;
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.email = email;
+//        this.username = username;
+//        this.password = password;
+//    }
+//
+//    public int getUser_id() {
+//        return user_id;
+//    }
+//
+//    public void setUser_id(int user_id) {
+//        this.user_id = user_id;
+//    }
+//
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getUsername() {
+//        return this.username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "user_id=" + user_id +
+//                ", firstname='" + firstname + '\'' +
+//                ", lastname='" + lastname + '\'' +
+//                ", email='" + email + '\'' +
+//                ", username='" + username + '\'' +
+//                ", password='" + password + '\'' +
+//                '}';
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return user_id == user.user_id && Objects.equals(firstname, user.firstname) && Objects.equals(lastname, user.lastname) && Objects.equals(email, user.email) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(user_id, firstname, lastname, email, username, password);
+//    }
+//
+//    public Object getQuestionText() {
+//        return null;
+//    }
+//
+//    public Object getAnswerText() {
+//        return null;
+//    }
 }
